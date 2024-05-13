f7463141d758c61201014a6d17f8b3fb889a5fb9
jaganoer-works <jaganoer@gmail.com>
2024-05-14 08:36:21 +0900

    auth0の認可を使用

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 module.exports.handler = async (event) => {
   return {
-    // statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "https://www.example.com",
-      "Access-Control-Allow-Credentials": true,
-    },
+    statusCode: 200,
     body: JSON.stringify(
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        // input: event.version,
       },
       null,
       2
