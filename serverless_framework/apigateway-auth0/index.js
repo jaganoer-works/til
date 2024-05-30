9a03cc0a5cb95026fabfb59bff77fcd7031cab30
jaganoer-works <jaganoer@gmail.com>
2024-05-31 02:39:09 +0900

    Access-Control-Allow-Originを追加

--- serverless_framework/apigateway-auth0/index.js
+++ serverless_framework/apigateway-auth0/index.js
@@ -4,7 +4,9 @@ module.exports.handler = async (event) => {
     body: JSON.stringify(
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
-        // input: event.version,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
       },
       null,
       2
